16d27dc29bd860a2988aebd67288ac342c0665a4
MsLVictor <victormleite25@icloud.com>
2023-09-02 13:47:39 -0300

    feat: criação da class ContaTerminal concluida

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        try (// criando scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, digite o número da Agência!");
+            int agencia = scanner.nextInt();
+
+            System.out.println("Digite sua Conta!");
+            String conta = scanner.next();
+
+            System.out.println("Digite seu Nome completo!");
+            String nomeCompleto = scanner.next();
+
+            System.out.println("Digite o valor a Depositar");
+            float valor = scanner.nextFloat();
+
+            System.out.println("Olá " + nomeCompleto + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + valor + " já está disponível para saque");
+        }
+
     }
 }
